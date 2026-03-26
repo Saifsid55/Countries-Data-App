38d159f0fd32095e471ea2491781dc93ee85022e
Saifsid55 <68994837+Saifsid55@users.noreply.github.com>
2026-03-27 02:19:43 +0530

    Add UIKit country detail navigation from home list

--- Countries Data App/Presentation/UIKit/HomeViewController.swift
+++ Countries Data App/Presentation/UIKit/HomeViewController.swift
@@ -130,6 +130,17 @@ extension HomeViewController: UITableViewDataSource {
 
 // MARK: Scroll fading effect
 extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let country = viewModel.dataByIndex(index: indexPath.row) else { return }
+        showDetail(for: country)
+    }
+
+    private func showDetail(for country: Country) {
+        let detailViewController = CountryDetailViewController(country: country)
+        navigationController?.pushViewController(detailViewController, animated: true)
+    }
+
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let offsetY = scrollView.contentOffset.y
         let animationRange: CGFloat = 80
@@ -168,5 +179,56 @@ extension HomeViewController: UITableViewDelegate {
         view.layoutIfNeeded()
         view.setNeedsLayout()
         lastOffsetY = offsetY
+    }
+}
+
+final class CountryDetailViewController: UIViewController {
+    private let country: Country
+
+    init(country: Country) {
+        self.country = country
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        title = country.name.common
+        navigationItem.largeTitleDisplayMode = .never
+
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.alignment = .leading
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        let officialNameLabel = UILabel()
+        officialNameLabel.text = country.name.official
+        officialNameLabel.font = .preferredFont(forTextStyle: .headline)
+        officialNameLabel.numberOfLines = 0
+
+        let flagLabel = UILabel()
+        flagLabel.text = country.flag
+        flagLabel.font = .preferredFont(forTextStyle: .largeTitle)
+
+        let capitalLabel = UILabel()
+        capitalLabel.text = "Capital: \(country.capital.first ?? "N/A")"
+        capitalLabel.font = .preferredFont(forTextStyle: .subheadline)
+        capitalLabel.numberOfLines = 0
+
+        [officialNameLabel, flagLabel, capitalLabel].forEach(stackView.addArrangedSubview)
+
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -16)
+        ])
     }
 }
